76dd23abd6ecdc01bc04e0c4f484bc5583a57418
wangwenbin <wangwenbin@outlook.com>
2014-05-15 20:46:32 +0800

    new file:   test.go

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	termbox.SetCell(0, 0, 'c', 0, 0)
+	termbox.SetCell(0, 0, 'c', 0, 0)
+	termbox.Flush()
+	termbox.PollEvent()
+
+}
